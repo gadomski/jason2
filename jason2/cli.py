9e17a2f2fb859fa021c5ff6f641cc37e4cccbaa0
Pete Gadomski <pete.gadomski@gmail.com>
2015-05-02 13:53:17 -0500

    Working on better configuration handling

--- jason2/cli.py
+++ jason2/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import ConfigParser
+import os
 
 from jason2.project import Project
 
@@ -8,12 +10,39 @@ def fetch(project, args):
 
 
 def parse_args():
+    config = ConfigParser.ConfigParser(defaults={
+        "data-directory": None,
+        "email": None,
+        "products": [],
+        "passes": [],
+        "start-cycle": None,
+        "end-cycle": None,
+    })
+    config.read(["jason2.cfg", os.path.expanduser("~/.jason2.cfg")])
+    if not config.has_section("project"):
+        config.add_section("project")
+
     parser = argparse.ArgumentParser(description="Download and analyze "
                                      "Jason2 data")
-
-    parser.add_argument("--config", default="jason2.cfg",
-                        help="Project configuration file "
-                             "(default: 'jason2.cfg')")
+    parser.add_argument("-d", "--data-directory",
+                        default=config.get("project", "data-directory"),
+                        help="The root directory for raw jason2 data")
+    parser.add_argument("-e", "--email",
+                        default=config.get("project", "email"),
+                        help="Email address to identify you when downloading "
+                             "data")
+    parser.add_argument("-p", "--products",
+                        default=config.get("project", "products"),
+                        help="A comma-delimted list of product names")
+    parser.add_argument("-t", "--passes",
+                        default=config.get("project", "passes"),
+                        help="A comma-delimted list of pass numbers")
+    parser.add_argument("--start-cycle",
+                        default=config.get("project", "start-cycle"),
+                        help="The first cycle to download")
+    parser.add_argument("--end-cycle",
+                        default=config.get("project", "end-cycle"),
+                        help="The last cycle to download")
 
     subparsers = parser.add_subparsers()
 
--- jason2/project.py
+++ jason2/project.py
@@ -1,24 +1,22 @@
-import ConfigParser
-
 from jason2.ftp import FtpConnection
-from jason2.system import FIRST_CYCLE, LAST_CYCLE
 
 
 class Project(object):
     """Holds project configuration parameters, such as data directory."""
 
-    def __init__(self, data_directory, products, passes, email,
-                 start_cycle=FIRST_CYCLE, end_cycle=LAST_CYCLE):
-        self.data_directory = data_directory
-        self.products = products
-        self.passes = passes
-        self.email = email
-        self.start_cycle = start_cycle
-        self.end_cycle = end_cycle
+    def __init__(self):
+        self.data_directory = "."
+        self.email = None
+        self.products = []
+        self.passes = []
+        self.start_cycle = None
+        self.end_cycle = None
 
     def fetch(self, skip_unzipping=False):
         with FtpConnection(self.email) as ftp:
             for product in self.products:
-                for cycle in range(self.start_cycle, self.end_cycle + 1):
+                cycle_range = ftp.get_cycle_range(self.start_cycle,
+                                                  self.end_cycle)
+                for cycle in cycle_range:
                     ftp.fetch(product, cycle, self.passes, self.data_directory,
                               skip_unzipping)
