e6c774c13a01f2cbe09947c39c1dac7b4989bebc
Pete Gadomski <pete.gadomski@gmail.com>
2015-05-02 07:37:02 -0500

    Add from_configfile method to Project

--- jason2/project.py
+++ jason2/project.py
@@ -1,5 +1,14 @@
+import ConfigParser
+
+
 class Project(object):
     """Holds project configuration parameters, such as data directory."""
+
+    @classmethod
+    def from_config(cls, filename):
+        config = ConfigParser.RawConfigParser()
+        config.read(filename)
+        return cls(config.get("data", "directory"))
 
     def __init__(self, data_directory):
         self.data_directory = data_directory
