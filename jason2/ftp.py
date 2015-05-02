547eb2cdd885ced7e79bbf9b21a7bbebe8632a2c
Pete Gadomski <pete.gadomski@gmail.com>
2015-05-02 08:54:43 -0500

    Fetching might work

--- jason2/ftp.py
+++ jason2/ftp.py
@@ -1,8 +1,26 @@
+import fnmatch
 import ftplib
 import os
 
 from jason2.exceptions import ConnectionError
-from jason2.utils import get_jason2_filename
+
+
+def zfill3(integer):
+    return str(integer).zfill(3)
+
+
+def jason2_glob(product, cycle, track):
+    # FIXME this is way too dumb
+    if product == "gdr_d":
+        product_type = "N"
+    elif product == "sgdr_d":
+        product_type = "S"
+    cycle_str = zfill3(cycle)
+    track_str = zfill3(track)
+    # FIXME also way too dumb
+    extension = ".nc" if product == "gdr_d" else ".zip"
+    return "JA2_GP{}_2PdP{}_{}_*{}".format(product_type, cycle_str, track_str,
+                                           extension)
 
 
 class FtpConnection(object):
@@ -26,11 +44,14 @@ class FtpConnection(object):
     def fetch(self, product, cycle, tracks, data_directory):
         if self.connection is None:
             raise ConnectionError("Not connected to FTP server")
-        cycle_str = str(cycle).zfill(3)
+        cycle_str = zfill3(cycle)
         self.connection.cwd(os.path.join(self.ROOT_PATH, product,
                                          cycle_str))
         for track in tracks:
-            filename = get_jason2_filename(product, cycle, track)
+            glob = jason2_glob(product, cycle, track)
+            filenames = fnmatch.filter(self.connection.nlist(), glob)
+            assert len(filenames) == 1
+            filename = filenames[0]
             outfile = os.path.join(data_directory, product, cycle_str,
                                    filename)
             self.connection.retrbinary("RETR {}".format(filename),
--- jason2/utils.py
+++ jason2/utils.py
@@ -1,6 +1,2 @@
 def str_to_list(string):
     return [s.strip() for s in string.split(",")]
-
-
-def get_jason2_filename(product, cycle, track):
-    pass
