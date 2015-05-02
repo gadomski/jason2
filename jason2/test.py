556af0e6b4915480be91b8829ec5213c6f6d21b8
Pete Gadomski <pete.gadomski@gmail.com>
2015-05-02 15:33:04 -0500

    Remove tests

--- jason2/test.py
+++ jason2/test.py
@@ -1,12 +1,5 @@
 from jason2.project import Project
-from jason2.ftp import jason2_glob
 
 
 def test_project2_constructor():
-    project = Project(".", ["gdr"], [195])
-    assert project.data_directory == "."
-    assert project.products == ["gdr"]
-
-def test_jason2_glob():
-    assert "JA2_GPN_2PdP012_034_*.nc" == jason2_glob("gdr_d", 12, 34)
-    assert "JA2_GPS_2PdP056_078_*.zip" == jason2_glob("sgdr_d", 56, 78)
+    Project()
