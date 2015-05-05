9e0232e3bf10e9325bf830960d9dc5776dfc8225
Pete Gadomski <pete.gadomski@gmail.com>
2015-05-05 09:07:47 -0500

    Privatize project methods that need pass
    Only pass number stuff should be exposed api

--- jason2/project.py
+++ jason2/project.py
@@ -23,18 +23,6 @@ class Project(object):
                 ftp.fetch_product(product, skip_unzipping=skip_unzipping,
                                   overwrite=overwrite)
 
-    def get_filename(self, product, cycle, pass_):
-        g = os.path.join(self.data_directory, product.directory_name,
-                         "cycle_{}".format(zfill3(cycle)),
-                         product.get_glob(cycle, pass_, unzipped_only=True))
-        files = glob.glob(g)
-        assert len(files) == 1
-        return files[0]
-
-    def get_dataset(self, product, cycle, pass_):
-        filename = self.get_filename(product, cycle, pass_)
-        return Dataset(filename, pass_.bounds)
-
     def get_waveforms(self, cycle, pass_number=None):
         if PRODUCTS["sgdr"] not in self.products:
             raise Jason2Error("Can get waveforms without sgdr product")
@@ -50,6 +38,18 @@ class Project(object):
             pass_ = self.passes[0]
         dataset = self.get_dataset(PRODUCTS["sgdr"], cycle, pass_)
         return dataset.get_waveforms()
+
+    def _get_dataset(self, product, cycle, pass_):
+        filename = self.get_filename(product, cycle, pass_)
+        return Dataset(filename, pass_.bounds)
+
+    def _get_filename(self, product, cycle, pass_):
+        g = os.path.join(self.data_directory, product.directory_name,
+                         "cycle_{}".format(zfill3(cycle)),
+                         product.get_glob(cycle, pass_, unzipped_only=True))
+        files = glob.glob(g)
+        assert len(files) == 1
+        return files[0]
 
     def _get_pass_by_number(self, number):
         return next(pass_ for pass_ in self.passes if pass_.number == number)
